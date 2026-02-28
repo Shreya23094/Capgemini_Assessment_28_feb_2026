dc64b2be839d4d28a891e40acfaa7b5c1924c006
Shreya Singh <118115266+Shreya23094@users.noreply.github.com>
2026-02-28 12:05:00 +0530

    Added code files

---
+++ src/test/java/org/example/StreamAPITest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class StreamAPITest {
+
+    @Test
+    void getStreamAPIResults(){
+        StreamAPI sapi = new StreamAPI();
+        sapi.streamAPIQuestions();
+    }
+}
